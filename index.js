738892fd3e7973a62affcd4c278302f2b117ddbc
Beerendan <brendanrbowen@gmail.com>
2023-06-26 12:05:23 -0400

    got input criteria set up

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 //Packages needed for this application
 const inquirer=require("inquirer");
 const fs=require("fs");
-const genMD=require("./utils/generateMarkdown");
+const generateMarkdown=require("./utils/generateMarkdown");
+const { validateHeaderName } = require("http");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//NEED: authorname, email, github username, project title, repo name, proj description, 
+//installation info, usage, screenshot link, credits and license
+const questions = ()=>{
+    return inquirer.prompt([
+        {
+            type:"input",
+            message:"FIrst and last name:",
+            name:"AuthorName",
+            validate:(name)=>{
+                let check=name.match(/^[a-zA-Z]+[a-zA-Z]+/g);
+                if (check) return true;
+                return "That is a made up name. What is your REAL name?"
+            },
+        },
+
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
